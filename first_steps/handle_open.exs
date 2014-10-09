335e781f939de8337fe60f91f3bad79ec48e622d
Dzianis Dashkevich <dskecse@gmail.com>
2014-10-09 20:07:03 +0300

    Add example on anonymous functions and pattern matching

---
+++ first_steps/handle_open.exs
@@ -0,0 +1,10 @@
+handle_open = fn
+  { :ok, file } -> "First line: #{IO.read(file, :line)}"
+  { _, error }  -> "Error: #{:file.format_error(error)}"
+end
+
+# call with a file that exists
+IO.puts handle_open.(File.open("intro/hello.exs"))
+
+# and then with one that does not
+IO.puts handle_open.(File.open("nonexistent"))
